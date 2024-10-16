c58e4452c0e00f963a94f279f1db4048735c2212
Bruno Rodrigues <brodriguesco@protonmail.com>
2024-10-16 11:14:39 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
  pkgs = import (fetchTarball "https://github.com/rstats-on-nix/nixpkgs/archive/refs/heads/REPLACE_DATE.tar.gz") {};
  system_packages = builtins.attrValues {
-  inherit (pkgs) R glibcLocalesUtf nix;
+  inherit (pkgs) R glibcLocales nix;
 };
  r_packages = builtins.attrValues {
   inherit (pkgs.rPackages)
@@ -12,7 +12,7 @@ let
 };
   in
   pkgs.mkShell {
-    LOCALE_ARCHIVE = if pkgs.system == "x86_64-linux" then  "${pkgs.glibcLocalesUtf}/lib/locale/locale-archive" else "";
+    LOCALE_ARCHIVE = if pkgs.system == "x86_64-linux" then  "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
     LANG = "en_US.UTF-8";
     LC_ALL = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
