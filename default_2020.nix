43483d041727b802bc545c88e4bd118c3bebc7e3
Bruno Rodrigues <bruno@brodrigues.co>
2024-12-16 20:53:00 +0100

    updated workflow file to handle different years

---
+++ default_2020.nix
@@ -0,0 +1,26 @@
+let
+ pkgs = import (fetchTarball "https://github.com/rstats-on-nix/nixpkgs/archive/refs/heads/REPLACE_DATE.tar.gz") {};
+ system_packages = builtins.attrValues {
+  inherit (pkgs) R glibcLocales nix;
+};
+ r_packages = builtins.attrValues {
+  inherit (pkgs.rPackages)
+    dplyr
+    ;
+};
+ wrapped_pkgs = pkgs.rWrapper.override {
+  packages = [ r_packages ];
+};
+  in
+  pkgs.mkShell {
+    LOCALE_ARCHIVE = if pkgs.system == "x86_64-linux" then  "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
+    LANG = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+
+    buildInputs = [ system_packages r_packages wrapped_pkgs ];
+
+  }
